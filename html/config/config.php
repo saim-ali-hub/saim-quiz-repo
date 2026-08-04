dad349921f159f378bf5bb1c31004382b5eac0d4
saim ali <saim.aliam2233@gmail.com>
2026-08-04 04:54:24 -0400

    Create config.php
    aa

---
+++ html/config/config.php
@@ -0,0 +1,21 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Active Directory Configuration
+|--------------------------------------------------------------------------
+*/
+
+define('LDAP_SERVER', '192.168.111.50');
+define('LDAP_PORT', 389);
+
+// Your Active Directory domain
+define('LDAP_DOMAIN', 'corp.linoop.us');
+
+// Base DN
+define('LDAP_BASE_DN', 'DC=corp,DC=linoop,DC=us');
+
+// Vcenter Server
+define('VCENTER_SERVER','192.168.111.190');
+
+
+?>
